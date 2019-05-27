f40966a22584a5d8516bd50fad8f126556201aa7
Rafael Lillo <rafael.andrade@sky.uk>
2019-05-27 11:32:32 +0100

    Finish sample

--- sample/Multi/Things/ExampleDimmableLight.cs
+++ sample/Multi/Things/ExampleDimmableLight.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Mozilla.IoT.WebThing;
 using Newtonsoft.Json.Linq;
+using Action = Mozilla.IoT.WebThing.Action;
 
 namespace Multi.Things
 {
@@ -8,8 +12,108 @@ namespace Multi.Things
         public ExampleDimmableLight() 
             : base("My Lamp", new JArray("OnOffSwitch", "Light"),
                 "A web connected lamp")
+        
         {
+            var onDescription = new JObject
+            {
+                {"@type", "OnOffProperty"},
+                {"title", "On/Off"},
+                {"type", "boolean"},
+                {"description", "Whether the lamp is turned on"}
+            };
+
+            var on = new Property(this, "on", true, onDescription);
+            on.ValuedChanged += (sender, args) =>
+            {
+                Console.WriteLine($"On-State is now {args.Value}");
+            };
+    
+            AddProperty(on);
+
+            var brightnessDescription = new JObject
+            {
+                {"@type", "BrightnessProperty"},
+                {"title", "Brightness"},
+                {"type", "integer"},
+                {"description", "The level of light from 0-100"},
+                {"minimum", 0},
+                {"maximum", 100},
+                {"unit", "percent"}
+            };
             
+            var brightness  = new Property<double>(this, "brightness", 50, brightnessDescription);
+            brightness.ValuedChanged += (sender, args) =>
+            {
+                Console.WriteLine($"Brightness is now {args.Value}");
+            };
+            
+            AddProperty(brightness);
+
+
+            var fadeMetadata = new JObject
+            {
+                {"title", "Fade"}, 
+                {"description", "Fade the lamp to a given level"}
+            };
+            
+            var fadeInAdd = new JObject
+            {
+                {"type", "object"}, 
+                {"required", new JArray("brightness", "duration")}
+            };
+
+            var fadeBrightness = new JObject
+            {
+                {"type", "integer"}, 
+                {"minimum", 0}, 
+                {"maximum", 100}, 
+                {"unit", "percent"}
+            };
+
+            var fadeDuration = new JObject
+            {
+                {"type", "integer"}, 
+                {"minimum", 1}, 
+                {"unit", "milliseconds"}
+            };
+
+            var fadeProperties = new JObject
+            {
+                {"brightness", fadeBrightness}, 
+                {"duration", fadeDuration}
+            };
+            
+            fadeInAdd.Add("properties", fadeProperties);
+            fadeMetadata.Add("inAdd", fadeInAdd);
+            
+            AddAvailableAction<FadeAction>("fade", fadeMetadata);
+        }
+        
+        public class OverheatedEvent : Event
+        {
+            public OverheatedEvent(Thing thing, int data) 
+                : base(thing, "overheated", data)
+            {
+                
+            }
+        }
+        
+        public class FadeAction : Action
+        {
+            public FadeAction(Thing thing, JObject input) 
+                : base(thing, input)
+            {
+            }
+
+            public override string Id { get; } = Guid.NewGuid().ToString();
+            public override string Name => "fade";
+
+            protected override async Task PerformActionAsync(CancellationToken cancellation)
+            {
+                await Task.Delay(Input.Value<int>("input"), cancellation);
+                Thing.SetProperty("brightness", Input.Value<int>("brightness"));
+                await Thing.AddEventAsync(new OverheatedEvent(Thing, 102), cancellation);
+            }
         }
     }
 }
--- sample/Multi/Things/FakeGpioHumiditySensor.cs
+++ sample/Multi/Things/FakeGpioHumiditySensor.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Mozilla.IoT.WebThing;
 using Newtonsoft.Json.Linq;
 
@@ -5,13 +7,40 @@ namespace Multi.Things
 {
     public class FakeGpioHumiditySensor : Thing
     {
+        private readonly Property<double> _level;  
+        private static Random s_random = new Random();
         public FakeGpioHumiditySensor()
             : base("My Humidity Sensor",
                 new JArray("MultiLevelSensor"),
                 "A web connected humidity sensor")
         {
-            
+            var levelDescription = new JObject
+            {
+                {"@type", "LevelProperty"},
+                {"title", "Humidity"},
+                {"type", "number"},
+                {"description", "The current humidity in %"},
+                {"minimum", 0},
+                {"maximum", 100},
+                {"unit", "percent"},
+                {"readOnly", true}
+            };
+
+            _level = new Property<double>(this, "level", 0, levelDescription);
+
+            Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(3_000);
+                double value = ReadFromGPIO();
+                Console.WriteLine($"setting new humidity level: {value}");
+                //_level.NotifyOfExternalUpdate(value);
+            });
         }
         
+        /**
+         * Mimic an actual sensor updating its reading every couple seconds.
+         */
+        private static double ReadFromGPIO()
+            => Math.Abs(70.0d * s_random.Next() * (-0.5 + s_random.Next()));
     }
 }
