9cba585dbcf12a8e489d8a5a5bf3246efdc9e6d8
Rafael Lillo <rafa_lillo@outlook.com>
2019-06-14 21:40:17 +0100

    Add get test

--- test/Mozilla.IoT.WebThing.Accepted.Test/SingleTest.cs
+++ test/Mozilla.IoT.WebThing.Accepted.Test/SingleTest.cs
@@ -1,9 +1,8 @@
-using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using AutoFixture;
 using FluentAssertions;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Mozilla.IoT.WebThing.Accepted.Test.Startups;
 using Newtonsoft.Json.Linq;
@@ -13,22 +12,25 @@ namespace Mozilla.IoT.WebThing.Accepted.Test
 {
     public class SingleTest
     {
+        private readonly Fixture _fixture;
         private readonly TestServer _server;
         private readonly HttpClient _httpClient;
 
 
         public SingleTest()
         {
+            _fixture = new Fixture();
             _server = new TestServer(WebHostBuilder.Create<SingleStartup>());
             _httpClient = _server.CreateClient();
         }
-        
+
+        #region Get
         [Fact]
-        public async Task GetAsync()
+        public async Task Get()
         {
             var responseMessage = await _httpClient.GetAsync("/");
             responseMessage.IsSuccessStatusCode.Should().BeTrue();
-            string  json = await responseMessage.Content.ReadAsStringAsync();
+            string json = await responseMessage.Content.ReadAsStringAsync();
             json.Should().NotBeNullOrEmpty();
             var expected = JObject.Parse(@"{
                 ""name"": ""My Lamp"",
@@ -85,16 +87,16 @@ namespace Mozilla.IoT.WebThing.Accepted.Test
                     ""href"": """"
                 }]
             }");
-            
+
             JToken.DeepEquals(JObject.Parse(json), expected).Should().BeTrue();
         }
 
         [Fact]
-        public async Task GetPropertiesAsync()
+        public async Task GetProperties()
         {
             var responseMessage = await _httpClient.GetAsync("/properties");
             responseMessage.IsSuccessStatusCode.Should().BeTrue();
-            string  json = await responseMessage.Content.ReadAsStringAsync();
+            string json = await responseMessage.Content.ReadAsStringAsync();
             json.Should().NotBeNullOrEmpty();
             var expected = JObject.Parse(@"{
                 ""on"": {
@@ -125,19 +127,37 @@ namespace Mozilla.IoT.WebThing.Accepted.Test
 
             JToken.DeepEquals(JObject.Parse(json), expected).Should().BeTrue();
         }
-        
+
         [Theory]
         [InlineData("on", @"{ ""on"": true }")]
         [InlineData("level", @"{ ""level"": 0.0 }")]
-        public async Task GetPropertyAsync(string property, string expectedJson)
+        public async Task GetProperty(string property, string expectedJson)
         {
             var responseMessage = await _httpClient.GetAsync($"/properties/{property}");
             responseMessage.IsSuccessStatusCode.Should().BeTrue();
             string json = await responseMessage.Content.ReadAsStringAsync();
             json.Should().NotBeNullOrEmpty();
             var expected = JObject.Parse(expectedJson);
-
             JToken.DeepEquals(JObject.Parse(json), expected).Should().BeTrue();
         }
+        
+        [Fact]
+        public async Task GetProperty_NotFound()
+        {
+            var responseMessage = await _httpClient.GetAsync($"/properties/{_fixture.Create<string>()}");
+            responseMessage.IsSuccessStatusCode.Should().BeFalse();
+            Assert.Equal(HttpStatusCode.NotFound, responseMessage.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAction()
+        {
+            var responseMessage = await _httpClient.GetAsync($"/actions");
+            responseMessage.IsSuccessStatusCode.Should().BeTrue();
+            string json = await responseMessage.Content.ReadAsStringAsync();
+            json.Should().NotBeNullOrEmpty();
+            json.Should().Be("[]");
+        }
+        #endregion
     }
 }
