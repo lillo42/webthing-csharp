9587fca7e4f1c16e00c9a3769b127927a8c115fe
Rafael Lillo <rafa_lillo@outlook.com>
2020-03-13 23:15:59 +0000

    Add Action test

--- src/Mozilla.IoT.WebThing/Actions/ActionCollection.cs
+++ src/Mozilla.IoT.WebThing/Actions/ActionCollection.cs
@@ -9,12 +9,12 @@ namespace Mozilla.IoT.WebThing.Actions
     public class ActionCollection : IEnumerable<ActionInfo>
     {
         private readonly ConcurrentDictionary<Guid, ActionInfo> _actions;
-        private readonly InfoConvert _inputConvert;
+        private readonly ActionInfoConvert _inputConvert;
         private readonly IActionInfoFactory _actionInfoFactory;
 
         public event EventHandler<ActionInfo>? Change;
 
-        public ActionCollection(InfoConvert inputConvert, IActionInfoFactory actionInfoFactory)
+        public ActionCollection(ActionInfoConvert inputConvert, IActionInfoFactory actionInfoFactory)
         {
             _actionInfoFactory = actionInfoFactory ?? throw new ArgumentNullException(nameof(actionInfoFactory));
             _inputConvert = inputConvert;
@@ -35,6 +35,8 @@ namespace Mozilla.IoT.WebThing.Actions
             }
 
             info = _actionInfoFactory.CreateActionInfo(inputValues);
+            info.StatusChanged += OnStatusChange;
+            
             return _actions.TryAdd(info.GetId(), info);
         }
 
---
+++ src/Mozilla.IoT.WebThing/Actions/ActionInfoConvert.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Mozilla.IoT.WebThing.Actions
+{
+    public readonly struct ActionInfoConvert
+    {
+        private readonly IReadOnlyDictionary<string, IActionParameter> _actionParameters;
+
+        public ActionInfoConvert(IReadOnlyDictionary<string, IActionParameter> actionParameters)
+        {
+            _actionParameters = actionParameters ?? throw new ArgumentNullException(nameof(actionParameters));
+        }
+
+        public bool TryConvert(JsonElement element, out Dictionary<string, object?> input)
+        {
+            input = new Dictionary<string, object?>(StringComparer.InvariantCultureIgnoreCase);
+
+            foreach (var properties in element.EnumerateObject())
+            {
+                if (!_actionParameters.TryGetValue(properties.Name, out var @params))
+                {
+                    input = null;
+                    return false;
+                }
+
+                if (!@params.TryGetValue(properties.Value, out var value))
+                {
+                    input = null;
+                    return false;
+                }
+                
+                if (!input.TryAdd(properties.Name, value))
+                {
+                    input = null;
+                    return false;
+                }
+            }
+
+            foreach (var (property, parameter) in _actionParameters)
+            {
+                if (!input.ContainsKey(property))
+                {
+                    if (!parameter.CanBeNull)
+                    {
+                        input = null;
+                        return false;
+                    }
+
+                    input.Add(property, null);
+                }
+            }
+
+            return true;
+        }
+    }
+}
