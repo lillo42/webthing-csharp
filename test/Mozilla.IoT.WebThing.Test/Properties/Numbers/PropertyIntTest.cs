09cd45e53476bef6c7bd3e425637f5f7ec8b235b
Rafael Lillo <rafa_lillo@outlook.com>
2020-03-13 21:13:42 +0000

    Add property number test

---
+++ test/Mozilla.IoT.WebThing.Test/Properties/Numbers/PropertyIntTest.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Text.Json;
+using AutoFixture;
+using FluentAssertions;
+using Mozilla.IoT.WebThing.Properties.Number;
+using Xunit;
+
+namespace Mozilla.IoT.WebThing.Test.Properties.Numbers
+{
+    public class PropertyIntTest
+    {
+        private readonly NumberThing _thing;
+        private readonly Fixture _fixture;
+        
+        public PropertyIntTest()
+        {
+            _fixture = new Fixture();
+            _thing = new NumberThing();
+        }
+        
+        #region No Nullable 
+        private PropertyInt CreateNoNullable(int[]? enums = null, int? min = null, int? max = null, int? multipleOf = null)
+            => new PropertyInt(_thing,
+                thing => ((NumberThing)thing).Number,
+                (thing, value) => ((NumberThing)thing).Number = (int)value,
+                false, min, max, multipleOf, enums);
+
+        [Fact]
+        public void SetNoNullableWithValue()
+        {
+            var value = _fixture.Create<int>();
+            var property = CreateNoNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNoNullableWithValueEnums()
+        {
+            var values = _fixture.Create<int[]>();
+            var property = CreateNoNullable(values);
+            foreach (var value in values)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+                property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+                _thing.Number.Should().Be(value);
+            }
+        }
+        
+        [Theory]
+        [InlineData(11)]
+        [InlineData(10)]
+        public void SetNoNullableWithMinValue(int value)
+        {
+            var property = CreateNoNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(value);
+        }
+        
+        [Theory]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void SetNoNullableWithMaxValue(int value)
+        {
+            var property = CreateNoNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNoNullableWithMultipleOfValue()
+        {
+            var property = CreateNoNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 10 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(10);
+        }
+        
+        [Fact]
+        public void TrySetNoNullableWithNullValue()
+        {
+            var property = CreateNoNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(@"{ ""input"": null }");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(typeof(bool))]
+        [InlineData(typeof(string))]
+        public void TrySetNoNullableWitInvalidValue(Type type)
+        {
+            var value = type == typeof(bool) ? _fixture.Create<bool>().ToString().ToLower() : $@"""{_fixture.Create<string>()}""";
+            var property = CreateNoNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNoNullableWithEnumValue()
+        {
+            var values = _fixture.Create<int[]>();
+            var property = CreateNoNullable(values);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {_fixture.Create<int>()} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void TrySetNoNullableWithMinValue(int value)
+        {
+            var property = CreateNoNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(12)]
+        [InlineData(11)]
+        public void TrySetNoNullableWithMaxValue(int value)
+        {
+            var property = CreateNoNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNoNullableWithMultipleOfValue()
+        {
+            var property = CreateNoNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 9 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        #endregion
+        
+        #region Nullable
+
+        private PropertyInt CreateNullable(int[]? enums = null, int? min = null, int? max = null, int? multipleOf = null)
+            => new PropertyInt(_thing,
+                thing => ((NumberThing)thing).NullableNumber,
+                (thing, value) => ((NumberThing)thing).NullableNumber = (int?)value,
+                true, min, max, multipleOf, enums);
+
+        [Fact]
+        public void SetNullableWithValue()
+        {
+            var value = _fixture.Create<int>();
+            var property = CreateNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().NotBeNull();
+            _thing.NullableNumber.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNullableWithNullValue()
+        {
+            var property = CreateNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(@"{ ""input"": null }");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().BeNull();
+        }
+        
+        [Fact]
+        public void SetNullableWithValueEnums()
+        {
+            var values = _fixture.Create<int[]>();
+            var property = CreateNullable(values);
+            foreach (var value in values)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+                property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+                _thing.NullableNumber.Should().Be(value);
+            }
+        }
+        
+        [Theory]
+        [InlineData(11)]
+        [InlineData(10)]
+        public void SetNullableWithMinValue(int value)
+        {
+            var property = CreateNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().Be(value);
+        }
+        
+        [Theory]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void SetNullableWithMaxValue(int value)
+        {
+            var property = CreateNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNullableWithMultipleOfValue()
+        {
+            var property = CreateNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 10 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().Be(10);
+        }
+        
+        [Theory]
+        [InlineData(typeof(bool))]
+        [InlineData(typeof(string))]
+        public void TrySetNullableWitInvalidValue(Type type)
+        {
+            var value = type == typeof(bool) ? _fixture.Create<bool>().ToString().ToLower() : $@"""{_fixture.Create<string>()}""";
+            var property = CreateNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNullableWitInvalidValueAndNotHaveValueInEnum()
+        {
+            var values = _fixture.Create<int[]>();
+            var property = CreateNullable(values);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {_fixture.Create<int>()} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void TrySetNullableWithMinValue(int value)
+        {
+            var property = CreateNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(12)]
+        [InlineData(11)]
+        public void TrySetNullableWithMaxValue(int value)
+        {
+            var property = CreateNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNullableWithMultipleOfValue()
+        {
+            var property = CreateNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 9 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        #endregion
+        
+        public class NumberThing : Thing
+        {
+            public override string Name => "number-thing";
+            
+            public int Number { get; set; }
+            public int? NullableNumber { get; set; }
+        }
+    }
+}
---
+++ test/Mozilla.IoT.WebThing.Test/Properties/Numbers/PropertyULongTest.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Text.Json;
+using AutoFixture;
+using FluentAssertions;
+using Mozilla.IoT.WebThing.Properties.Number;
+using Xunit;
+
+namespace Mozilla.IoT.WebThing.Test.Properties.Numbers
+{
+    public class PropertyULongTest
+    {
+        private readonly NumberThing _thing;
+        private readonly Fixture _fixture;
+        
+        public PropertyULongTest()
+        {
+            _fixture = new Fixture();
+            _thing = new NumberThing();
+        }
+        
+        #region No Nullable 
+        private PropertyULong CreateNoNullable(ulong[]? enums = null, ulong? min = null, ulong? max = null, ulong? multipleOf = null)
+            => new PropertyULong(_thing,
+                thing => ((NumberThing)thing).Number,
+                (thing, value) => ((NumberThing)thing).Number = (ulong)value,
+                false, min, max, multipleOf, enums);
+
+        [Fact]
+        public void SetNoNullableWithValue()
+        {
+            var value = _fixture.Create<ulong>();
+            var property = CreateNoNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNoNullableWithValueEnums()
+        {
+            var values = _fixture.Create<ulong[]>();
+            var property = CreateNoNullable(values);
+            foreach (var value in values)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+                property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+                _thing.Number.Should().Be(value);
+            }
+        }
+        
+        [Theory]
+        [InlineData(11)]
+        [InlineData(10)]
+        public void SetNoNullableWithMinValue(ulong value)
+        {
+            var property = CreateNoNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(value);
+        }
+        
+        [Theory]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void SetNoNullableWithMaxValue(ulong value)
+        {
+            var property = CreateNoNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNoNullableWithMultipleOfValue()
+        {
+            var property = CreateNoNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 10 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.Number.Should().Be(10);
+        }
+        
+        [Fact]
+        public void TrySetNoNullableWithNullValue()
+        {
+            var property = CreateNoNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(@"{ ""input"": null }");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(typeof(bool))]
+        [InlineData(typeof(string))]
+        public void TrySetNoNullableWitInvalidValue(Type type)
+        {
+            var value = type == typeof(bool) ? _fixture.Create<bool>().ToString().ToLower() : $@"""{_fixture.Create<string>()}""";
+            var property = CreateNoNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNoNullableWithEnumValue()
+        {
+            var values = _fixture.Create<ulong[]>();
+            var property = CreateNoNullable(values);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {_fixture.Create<ulong>()} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void TrySetNoNullableWithMinValue(ulong value)
+        {
+            var property = CreateNoNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(12)]
+        [InlineData(11)]
+        public void TrySetNoNullableWithMaxValue(ulong value)
+        {
+            var property = CreateNoNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNoNullableWithMultipleOfValue()
+        {
+            var property = CreateNoNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 9 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        #endregion
+        
+        #region Nullable
+
+        private PropertyULong CreateNullable(ulong[]? enums = null, ulong? min = null, ulong? max = null, ulong? multipleOf = null)
+            => new PropertyULong(_thing,
+                thing => ((NumberThing)thing).NullableNumber,
+                (thing, value) => ((NumberThing)thing).NullableNumber = (ulong?)value,
+                true, min, max, multipleOf, enums);
+
+        [Fact]
+        public void SetNullableWithValue()
+        {
+            var value = _fixture.Create<ulong>();
+            var property = CreateNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().NotBeNull();
+            _thing.NullableNumber.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNullableWithNullValue()
+        {
+            var property = CreateNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(@"{ ""input"": null }");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().BeNull();
+        }
+        
+        [Fact]
+        public void SetNullableWithValueEnums()
+        {
+            var values = _fixture.Create<ulong[]>();
+            var property = CreateNullable(values);
+            foreach (var value in values)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+                property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+                _thing.NullableNumber.Should().Be(value);
+            }
+        }
+        
+        [Theory]
+        [InlineData(11)]
+        [InlineData(10)]
+        public void SetNullableWithMinValue(ulong value)
+        {
+            var property = CreateNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().Be(value);
+        }
+        
+        [Theory]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void SetNullableWithMaxValue(ulong value)
+        {
+            var property = CreateNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().Be(value);
+        }
+        
+        [Fact]
+        public void SetNullableWithMultipleOfValue()
+        {
+            var property = CreateNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 10 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
+            _thing.NullableNumber.Should().Be(10);
+        }
+        
+        [Theory]
+        [InlineData(typeof(bool))]
+        [InlineData(typeof(string))]
+        public void TrySetNullableWitInvalidValue(Type type)
+        {
+            var value = type == typeof(bool) ? _fixture.Create<bool>().ToString().ToLower() : $@"""{_fixture.Create<string>()}""";
+            var property = CreateNullable();
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNullableWitInvalidValueAndNotHaveValueInEnum()
+        {
+            var values = _fixture.Create<ulong[]>();
+            var property = CreateNullable(values);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {_fixture.Create<ulong>()} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void TrySetNullableWithMinValue(ulong value)
+        {
+            var property = CreateNullable(min: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Theory]
+        [InlineData(12)]
+        [InlineData(11)]
+        public void TrySetNullableWithMaxValue(ulong value)
+        {
+            var property = CreateNullable(max: 10);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": {value} }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        
+        [Fact]
+        public void TrySetNullableWithMultipleOfValue()
+        {
+            var property = CreateNullable(multipleOf: 2);
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": 9 }}");
+            property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
+        }
+        #endregion
+        
+        public class NumberThing : Thing
+        {
+            public override string Name => "number-thing";
+            
+            public ulong Number { get; set; }
+            public ulong? NullableNumber { get; set; }
+        }
+    }
+}
