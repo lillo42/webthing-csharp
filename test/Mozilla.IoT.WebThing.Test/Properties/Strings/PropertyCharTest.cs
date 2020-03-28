826a6f9b1eb2ef9f7bdbe6bf5424748df8c41b9c
Rafael Lillo <rafa_lillo@outlook.com>
2020-03-28 14:59:42 +0000

    Fixes char test

--- test/Mozilla.IoT.WebThing.Test/Properties/Strings/PropertyCharTest.cs
+++ test/Mozilla.IoT.WebThing.Test/Properties/Strings/PropertyCharTest.cs
@@ -18,7 +18,7 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
             _fixture = new Fixture();
             _thing = new CharThing();
         }
-        
+
         #region No Nullable
         private PropertyChar CreateNoNullable(char[] enums = null)
             => new PropertyChar(_thing,
@@ -31,7 +31,11 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
         {
             var value = _fixture.Create<char>();
             var property = CreateNoNullable();
-            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": ""{value}"" }}");
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(new Serializer{Input = value}, 
+                new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                }));
             property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
             _thing.Char.Should().Be(value);
         }
@@ -43,7 +47,11 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
             var property = CreateNoNullable(values);
             foreach (var value in values)
             {
-                var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": ""{value}"" }}");
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(new Serializer{Input = value}, 
+                    new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    }));
                 property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
                 _thing.Char.Should().Be(value);
             }
@@ -73,7 +81,12 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
         {
             var values = _fixture.Create<char[]>();
             var property = CreateNoNullable(values);
-            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": ""{_fixture.Create<char>()}"" }}");
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(
+                new Serializer{Input = _fixture.Create<char>()}, 
+                new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                }));
             property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
         }
         #endregion
@@ -91,7 +104,14 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
         {
             var value = _fixture.Create<char>();
             var property = CreateNullable();
-            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": ""{value}"" }}");
+
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(
+                new Serializer{Input = value}, 
+                new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                }));
+            
             property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
             _thing.NullableChar.Should().NotBeNull();
             _thing.NullableChar.Should().Be(value);
@@ -113,7 +133,12 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
             var property = CreateNullable(values);
             foreach (var value in values)
             {
-                var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": ""{value}"" }}");
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(
+                    new Serializer{Input = value}, 
+                    new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    }));
                 property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.Ok);
                 _thing.NullableChar.Should().Be(value);
             }
@@ -135,7 +160,12 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
         {
             var values = _fixture.Create<char[]>();
             var property = CreateNullable(values);
-            var jsonElement = JsonSerializer.Deserialize<JsonElement>($@"{{ ""input"": ""{_fixture.Create<char>()}"" }}");
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(
+                new Serializer{Input = _fixture.Create<char>()}, 
+                new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                }));
             property.SetValue(jsonElement.GetProperty("input")).Should().Be(SetPropertyResult.InvalidValue);
         }
         
@@ -147,6 +177,11 @@ namespace Mozilla.IoT.WebThing.Test.Properties.Strings
             
             public char Char { get; set; }
             public char? NullableChar { get; set; }
+        }
+
+        public class Serializer
+        {
+            public char Input { get; set; }
         }
     }
 }
