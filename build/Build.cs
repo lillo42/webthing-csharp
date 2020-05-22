3a21aa7876de7838d5b7e2759efaa4d61f18234f
Rafael Andrade <rafael.andrade@sky.uk>
2020-05-22 16:02:57 +0100

    Add global.json and remove github action

--- build/Build.cs
+++ build/Build.cs
@@ -1,21 +1,27 @@
-using System;
-using System.Linq;
+using System.Collections.Generic;
+using System.IO;
 using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AzurePipelines;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.ReportGenerator;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.CompressionTasks;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
+[AzurePipelines(AzurePipelinesImage.UbuntuLatest,
+    AutoGenerate = true)]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -28,10 +34,25 @@ class Build : NukeBuild
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    
+    [Parameter("Nuget API key", Name = "api-key")] 
+    readonly string ApiKey;
+
+    [Parameter("NuGet Source for Packages", Name = "nuget-source")]
+    readonly string NugetSource = "https://api.nuget.org/v3/index.json";
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
     [GitVersion] readonly GitVersion GitVersion;
+    
+    AbsolutePath PackageDirectory => ArtifactsDirectory / "packages";
+    
+    AbsolutePath TestResultDirectory => ArtifactsDirectory / "test-results";
+    
+    string CoverageReportDirectory => ArtifactsDirectory / "coverage-report";
+    string CoverageReportArchive => ArtifactsDirectory / "coverage-report.zip";
+    
+    IEnumerable<Project> TestProjects => Solution.GetProjects("*.Test");
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
@@ -41,6 +62,9 @@ class Build : NukeBuild
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(s => s
+                .SetProject(Solution)
+                .SetConfiguration(Configuration));
             SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
             TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
             EnsureCleanDirectory(ArtifactsDirectory);
@@ -64,6 +88,81 @@ class Build : NukeBuild
                 .SetFileVersion(GitVersion.AssemblySemFileVer)
                 .SetInformationalVersion(GitVersion.InformationalVersion)
                 .EnableNoRestore());
+        });
+    
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Produces(TestResultDirectory / "*.trx")
+        .Produces(TestResultDirectory / "*.xml")
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .SetNoBuild(InvokedTargets.Contains(Compile))
+                .SetResultsDirectory(TestResultDirectory)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .When(InvokedTargets.Contains(Coverage) || IsServerBuild, _ => _
+                    .EnableCollectCoverage()
+                    .SetCoverletOutputFormat(CoverletOutputFormat.opencover)
+                    .When(IsServerBuild, _ => _.EnableUseSourceLink()))
+                .CombineWith(TestProjects, (_, v) => _
+                    .SetProjectFile(v)
+                    .SetLogger($"trx;LogFileName={v.Name}.trx")
+                    .SetCoverletOutput(TestResultDirectory / $"{v.Name}.xml")));
+        });
+
+    Target Coverage => _ => _
+        .DependsOn(Test)
+        .TriggeredBy(Test)
+        .Consumes(Test)
+        .Produces(CoverageReportArchive)
+        .Executes(() =>
+        {
+            if (InvokedTargets.Contains(Coverage) || IsServerBuild)
+            {
+                ReportGenerator(_ => _
+                    .SetReports(TestResultDirectory / "*.xml")
+                    .SetReportTypes(ReportTypes.HtmlInline)
+                    .SetTargetDirectory(CoverageReportDirectory)
+                    .SetFramework("netcoreapp2.1"));
+
+                CompressZip(
+                    directory: CoverageReportDirectory,
+                    archiveFile: CoverageReportArchive,
+                    fileMode: FileMode.Create);
+            }
+        });
+
+
+    Target Pack => _ => _
+        .DependsOn(Compile, Test)
+        .Produces(PackageDirectory / "*.nupkg")
+        .Produces(PackageDirectory / "*.snupkg")
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetProject(Solution)
+                .SetNoBuild(InvokedTargets.Contains(Compile))
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(PackageDirectory)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .EnableIncludeSource()
+                .EnableIncludeSymbols()
+                .EnableNoRestore());
+        });
+    
+    Target Publish => _ => _
+        .DependsOn(Clean, Test, Pack)
+        .Consumes(Pack)
+        .Requires(() => ApiKey)
+        .Requires(() => Configuration.Equals(Configuration.Release))
+        .Executes(() =>
+        {
+            DotNetNuGetPush(s => s
+                .SetApiKey(ApiKey)
+                .SetSkipDuplicate(true)
+                .SetSource(NugetSource));
         });
 
 }
